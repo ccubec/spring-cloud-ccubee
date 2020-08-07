aed83520c1ff56243e227a22858652e253f072e1
ccubee <ab9527@hotmail.com>
2020-08-08 07:18:50 +0800

    add: 测试检出

---
+++ ccubee-common/src/main/java/com/example/common/Test.java
@@ -0,0 +1,15 @@
+package com.example.common;
+
+import lombok.Data;
+
+/**
+ * @author ccubee
+ * @since 20-8-8 7:16
+ */
+@Data
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试检出");
+    }
+}
