f3b5e3c06d2386957cc4081744c4d2962a7f324c
ccubee <ab9527@hotmail.com>
2020-08-08 07:19:29 +0800

    add: 测试提交

---
+++ ccubee-common/src/main/java/com/example/common/TestDemo.java
@@ -0,0 +1,12 @@
+package com.example.common;
+
+/**
+ * @author ccubee
+ * @since 20-8-8 7:19
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("测试提交");
+    }
+}
