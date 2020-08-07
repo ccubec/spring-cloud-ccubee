a46dce8ae03f5528c526ed08ed93524c9ffdb950
ccubee <ab9527@hotmail.com>
2020-08-08 07:29:32 +0800

    add: 测试合并

--- ccubee-common/src/main/java/com/example/common/TestDemo.java
+++ ccubee-common/src/main/java/com/example/common/TestDemo.java
@@ -9,5 +9,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("测试提交");
         System.out.println("测试合并");
+        System.out.println("测试合并一");
     }
 }
